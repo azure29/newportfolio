54a5e880acebc9f5801789aebcc8c49bfd9b4872
Beatrice Saoro <beatricesaoro@Beatrices-MacBook-Air.local>
2016-05-29 22:55:29 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+	var offset = $('.nav-wrapper').outerHeight(true);
+	console.log(offset);
+	//let add a margin top to the body
+	$('body').css('margin-top', offset);
+
+	$('.nav-main a').on('click', function(event){
+		console.log(event);
+		event.preventDefault();
+		//scroll page associate with this
+		var pageToscrollTo = $(this).attr('href');
+		console.log(pageToscrollTo);
+		//using page to scroll in a .scroll function
+		$(window).scrollTo(pageToscrollTo, 1000, {offset: -offset});
+	});
+	//addig a click event to the logo-text
+	$('.js-nav').on('click', function() {
+		$('.nav-main').slideToggle();
+	});
+	$('.main-logo').on('mouseenter', function() {
+		$('.logo-text').fadeIn();
+	});
+	$('.main-logo').on('mouseleave', function() {
+		$('.logo-text').fadeOut();
+	});
+
+});//end doc .ready
